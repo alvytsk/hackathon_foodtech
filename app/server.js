603fffd7c74fc28e07f422e0b0888fcd68a31f99
Alexey Vymyatnin <alvy.tsk@gmail.com>
2018-12-07 23:02:31 +0700

    Add simple express server at 4000 port. To run server: npx nodemon server

---
+++ app/server.js
@@ -0,0 +1,8 @@
+var express = require('express');
+
+//  To run server run in console:
+//  npx nodemon server
+var app = express();
+var server = app.listen(4000, function(){
+  console.log('Listeing to requests on port 4000');
+})
